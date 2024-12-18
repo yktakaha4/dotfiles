f6ce5e5dc7b92f22ad6a55f3bbd254866a27609d
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-12-18 16:34:06 +0900

    fix

--- b1pro/code-server-proxy.sh
+++ b1pro/code-server-proxy.sh
@@ -6,16 +6,16 @@ base_dir="$(cd $(dirname $0); pwd)"
 
 $OAUTH2_PROXY_PATH \
     --http-address="127.0.0.1:18002" \
-    --upstream="http://127.0.0.1:18001/" \
+    --upstream="http://127.0.0.1:18001/code/" \
+    --proxy-prefix="/code/oauth2" \
     --email-domain="*" \
     --provider="github" \
     --github-user="$GITHUB_USERS" \
-    --redirect-url="https://b1pro:18000/oauth2/callback" \
+    --redirect-url="$REDIRECT_URL" \
     --client-id="$CLIENT_ID" \
     --client-secret="$CLIENT_SECRET" \
     --cookie-secret="$COOKIE_SECRET" \
     --cookie-secure=true \
     --cookie-name="_oauth2_proxy_code_server" \
     --proxy-websockets=true \
-    --proxy-prefix="/code" \
     --reverse-proxy=true
--- b1pro/code-server.sh
+++ b1pro/code-server.sh
@@ -3,6 +3,6 @@
 code serve-web \
     --host "127.0.0.1" \
     --port "18001" \
-    --server-base-path "/code" \
+    --server-base-path "/code/" \
     --without-connection-token \
     --accept-server-license-terms
