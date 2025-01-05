289200edb52efdfe61d92c5cddb7cc68e2b10ed6
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2025-01-05 20:48:17 +0900

    fix scripts

--- b1pro/cleanup.sh
+++ b1pro/cleanup.sh
@@ -15,7 +15,10 @@ echo "=== apt clean ==="
 apt clean
 
 echo "=== docker system prune ==="
-docker system prune -af
+docker system prune --filter "until=$((24 * 35))h" -af
+
+echo "=== delete temp files ==="
+rm -rvf /usr/share/nginx/html/shared/uploads/*
 
 echo "=== after ==="
 df -m
