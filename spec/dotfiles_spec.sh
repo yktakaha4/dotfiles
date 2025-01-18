5c1be026f0ee50e2f3f737f932cf543751f9d2a7
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2025-01-18 21:01:11 +0900

    fix

--- spec/dotfiles_spec.sh
+++ spec/dotfiles_spec.sh
@@ -7,6 +7,7 @@ Describe 'Integrated test'
 
     When call setup
     The output should equal ""
+    The error should equal ""
     The status should be success
     The variable EDITOR should equal vim
   End
