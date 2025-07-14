c4e5e2011256eaa5027c589d054416fa4a4dcd40
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2025-07-14 15:53:17 +0900

    fix

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ do
   if [ -e "$dst" ]; then
     echo "$fname: already exists"
   elif [ -e "$src" ]; then
+    mkdir -p "$(dirname "$dst")"
     ln -s "$src" "$dst"
     echo "$fname: create symlink"
   else
