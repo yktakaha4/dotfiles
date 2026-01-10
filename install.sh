b39c563b5e1ebda5ae330e1fc67a5378f36386ef
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2026-01-10 19:45:40 +0900

    refactor

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 base_dir="$(cd "$(dirname "$0")"; pwd)"
 install_dir=${DOTFILES_INSTALL_DIR:-"$HOME/.dotfiles"}
+target_dir=${DOTFILES_TARGET_DIR:-"$HOME"}
+skip_install_deps=${DOTFILES_SKIP_INSTALL_DEPS:-""}
 
 . "$base_dir/.helper.sh"
 
@@ -11,6 +13,7 @@ echo "--- directories ---"
 echo "os     : $(d_os)"
 echo "base   : $base_dir"
 echo "install: $install_dir"
+echo "target : $target_dir"
 
 if [ ! -e "$install_dir" ]; then
   ln -s "$base_dir" "$install_dir"
@@ -19,7 +22,11 @@ fi
 
 echo "--- install dependencies ---"
 
-"$base_dir/install.sh.$(d_os)"
+if [ -z "$skip_install_deps" ]; then
+  "$base_dir/install_deps.$(d_os).sh"
+else
+  echo "skipped."
+fi
 
 echo "--- setup dotfiles ---"
 
@@ -30,18 +37,26 @@ do
   else
     src="$install_dir/$sname"
   fi
-  dst="$HOME/$dname"
-  if [ -e "$dst" ]; then
-    echo "$dname: already exists"
-  elif [ -e "$src" ]; then
+  dst="$target_dir/$dname"
+  if [ -e "$src" ]; then
     mkdir -p "$(dirname "$dst")"
     if [ "$typ" = "cp" ]; then
-      rm -rvf "$dst"
-      cp -pr "$src" "$dst"
+      if [ -e "$dst" ]; then
+        echo "$dname: remove existing files"
+        rm -rvf "$dst"
+      fi
       echo "$dname: copy files"
+      cp -pr "$src" "$dst"
+      if [ -d "$dst" ]; then
+        touch "$dst/.managed_by_dotfiles"
+      fi
     elif [ "$typ" = "ln" ]; then
-      ln -s "$src" "$dst"
-      echo "$dname: create symlink"
+      if [ -e "$dst" ]; then
+        echo "$dname: already exists"
+      else
+        echo "$dname: create symlink"
+        ln -s "$src" "$dst"
+      fi
     else
       echo "invalid type: $typ"
       exit 1
