c5843eb66b35af3296d0b60774d948853b24e511
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2026-02-09 11:19:13 +0900

    add timeout script

--- .claude/hooks/timeout.bash
+++ .claude/hooks/timeout.bash
@@ -2,9 +2,11 @@
 
 set -euo pipefail
 
-tmux_pane_id="$TMUX_PANE"
 timeout=3
+timeout_message="ユーザーが一定時間操作を行わなかったため、自動的にキャンセルされました。提案した作業は実施できないものとして作業を継続してください。"
 send_key_delay=1
+
+tmux_pane_id="$TMUX_PANE"
 
 if [ -z "$tmux_pane_id" ]; then
     exit 0
@@ -12,17 +14,21 @@ fi
 
 initial_activity="$(tmux display-message -p -t "$tmux_pane_id" '#{pane_activity}')"
 
-sleep $timeout
+sleep "$timeout"
 
 current_activity="$(tmux display-message -p -t "$tmux_pane_id" '#{pane_activity}')"
 if [ "$initial_activity" != "$current_activity" ]; then
     exit 0
 fi
 
-tmux send-keys -t $tmux_pane_id Escape
-sleep $send_key_delay
+if [ ! tmux capture-pane -p -t "$tmux_pane_id" | grep -q "Esc to cancel" ]; then
+    exit 0
+fi
 
-tmux send-keys -t $tmux_pane_id "ユーザーが一定時間操作を行わなかったため、自動的にキャンセルされました。提案した作業は実施できないものとして作業を継続してください。"
-sleep $send_key_delay
+tmux send-keys -t "$tmux_pane_id" Escape
+sleep "$send_key_delay"
 
-tmux send-keys -t $tmux_pane_id Enter
+tmux send-keys -t "$tmux_pane_id" "$timeout_message"
+sleep "$send_key_delay"
+
+tmux send-keys -t "$tmux_pane_id" Enter
